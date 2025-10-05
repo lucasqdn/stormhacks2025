e3f33cd5bae88756e56785e6bf00704ea4c085a9
Brayden Chan <72172597+megabyte6@users.noreply.github.com>
2025-10-05 04:38:18 -0700

    Implement object detection and translation API endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from googletrans import Translator
+from model.yolov8 import detect_object
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+translator = Translator()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None) -> dict[str, int | str | None]:
-    return {"item_id": item_id, "q": q}
+class ProcessImageRequest(BaseModel):
+    src_lang: str
+    dest_lang: str
+    image: str
+
+
+@app.post("/process-image")
+def process_image(payload: ProcessImageRequest) -> dict[str, str]:
+    obj = detect_object(payload.image)
+
+    obj_src_lang = translator.translate(text=obj, dest=payload.src_lang).text
+    obj_dest_lang = translator.translate(text=obj, dest=payload.dest_lang).text
+
+    return {"src_lang_description": obj_src_lang, "dest_lang_description": obj_dest_lang}
